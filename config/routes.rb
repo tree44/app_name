5423d959b004e8b1016a5f43ffa297db3859c422
tree44 <tree44@gmail.com>
2012-11-08 09:29:19 -0600

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 AppName::Application.routes.draw do
   get "static_pages/home"
-
   get "static_pages/help"
+  get "static_pages/about"
 
   resources :microposts
-
   resources :users
 
   # The priority is based upon order of creation:
