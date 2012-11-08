0057a727b64aa7722d2726980ebab5b021288db5
tree44 <tree44@gmail.com>
2012-11-08 14:52:40 -0600

    finish signup

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "users/show" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :name => "Name",
+      :email => "Email",
+      :password => "Password",
+      :password_confirmation => "Password Confirmation"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Name/)
+    rendered.should match(/Email/)
+    rendered.should match(/Password/)
+    rendered.should match(/Password Confirmation/)
+  end
+end
